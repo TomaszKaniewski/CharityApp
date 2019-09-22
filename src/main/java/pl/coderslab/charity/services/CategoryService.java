b74109cb865b17cef96af0a326a1d8a0bfcc4d73
Tomasz Kaniewski <doktordryft@gmail.com>
2019-09-22 08:15:55 +0200

    # - rozszerzenie CategorySerwice

--- src/main/java/pl/coderslab/charity/services/CategoryService.java
+++ src/main/java/pl/coderslab/charity/services/CategoryService.java
@@ -1,7 +1,10 @@
 package pl.coderslab.charity.services;
 
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.repository.CategoryRepository;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -11,4 +14,13 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+
+    public List<Category> getCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).get();
+    }
+
 }
